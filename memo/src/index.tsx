bd2ac829e0026c1184b559494bf3e70aac0d8cf4
takumu <takumu155695@gmail.com>
2022-08-31 14:17:04 +0900

    Add: react-router-dom 6 を導入

--- memo/src/index.tsx
+++ memo/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { MemoProvider } from './compornents/providers/MemoProvider';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <MemoProvider>
-        <App/>
-    </MemoProvider>
+    <BrowserRouter>
+        <MemoProvider>
+            <App/>
+        </MemoProvider>
+    </BrowserRouter>
     ,
     document.getElementById('root')
 );
